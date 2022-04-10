6e591c11e70721d1d759373b469ee7cb2dddad2e
Egor Gorokh <gorokhegor@gmail.com>
2022-04-10 21:07:24 +0300

    First commit

---
+++ Task2.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Task;
+
+class Task2
+{
+    public static function main($date): string
+    {
+        $today = time();
+        $diff = strtotime($date) - $today;
+        return abs(round($diff / 86400));
+    }
+}
+Task2::main();
