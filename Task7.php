a9cb17d0ec1e95e3e628fb492904ff18fae69db7
Egor Gorokh <gorokhegor@gmail.com>
2022-04-11 21:21:15 +0300

    First commit

---
+++ Task7.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Task;
+
+class Task7
+{
+    public static function main($arr, $position)
+    {
+        if (!is_array($arr)) {
+            return "incorrect input data";
+        } elseif (!is_int($position)) {
+            return "incorrect input data";
+        } elseif (count($arr) <= $position) {
+            return "incorrect input data";
+        } else {
+            unset($arr[$position]);
+            $arr = array_values($arr);
+            return "$" . "arr" . "[" . implode(",", $arr) . "];";
+        }
+    }
+}
+
+Task7::main();
+
