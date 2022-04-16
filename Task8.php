f87b38226df26a0084b63d6fdb2c9e3eb8bdea02
Egor Gorokh <gorokhegor@gmail.com>
2022-04-16 14:16:50 +0300

    First commit

---
+++ Task8.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Task;
+
+class Task8
+{
+    public static function main($json)
+    {
+        if (!is_string($json) && !is_array(json_decode($json, true))) {
+            throw new Exception('incorrect input data');
+        }
+        function bookArray($value, $key)
+        {
+            print_r("$key: $value<br>");
+        }
+        $books = json_decode($json, true);
+        array_walk_recursive($books, 'Task\bookArray');
+    }
+}
+
+Task8::main('{
+"Title": "The Cuckoos Calling",
+"Author": "Robert Galbraith",
+"Detail": {
+"Publisher": "Little Brown"
+}
+}');
