03f2b8a8ac6ac16dc35645a461accf8260f06424
Egor Gorokh <gorokhegor@gmail.com>
2022-04-16 13:22:11 +0300

    Second commit

--- Task7.php
+++ Task7.php
@@ -7,18 +7,19 @@ class Task7
     public static function main($arr, $position)
     {
         if (!is_array($arr)) {
-            return "incorrect input data";
-        } elseif (!is_int($position)) {
-            return "incorrect input data";
-        } elseif (count($arr) <= $position) {
-            return "incorrect input data";
-        } else {
-            unset($arr[$position]);
-            $arr = array_values($arr);
-            return "$"."arr=[" . implode(",", $arr) . "];";
+            throw new Exception('incorrect input data');
         }
+        if (!is_int($position)) {
+            throw new Exception('incorrect input data');
+        }
+        if (count($arr) <= $position) {
+            throw new Exception('incorrect input data');
+        }
+        unset($arr[$position]);
+        $arr = array_values($arr);
+
+        return '$' . 'arr=[' . implode(',', $arr) . '];';
     }
 }
 
- Task7::main();
-
+// Task7::main([1,2,3],5);
