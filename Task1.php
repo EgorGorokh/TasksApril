f331be15abffde6f3f16f1a1d713199876746baa
Egor Gorokh <gorokhegor@gmail.com>
2022-04-16 12:33:18 +0300

    Second commit

--- Task1.php
+++ Task1.php
@@ -4,18 +4,20 @@ namespace Task;
 
 class Task1
 {
-    public static function main($inputNumber) : string
+    public static function main($inputNumber): string
     {
-        return !filter_var($inputNumber, FILTER_VALIDATE_INT) === TRUE
-            ? "incorrect input data"
-            : ($inputNumber > 30
-                ? "More than 30"
-                : ($inputNumber > 20
-                    ? "More than 20"
-                    : ($inputNumber > 10
-                        ? "More than 10"
-                        : "Less than 10")));
+        if (!is_int($inputNumber)) {
+            throw new Exception('incorrect input data');
+        }
+
+        return ($inputNumber > 30
+            ? 'More than 30'
+            : ($inputNumber > 20
+                ? 'More than 20'
+                : ($inputNumber > 10
+                    ? 'More than 10'
+                    : 'Less than 10')));
     }
 }
 
-Task1::main();
+//Task1::main();
