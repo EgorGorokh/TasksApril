bbb18d2825959b5048b42a7ca3a60e3c8fe33a87
Egor Gorokh <gorokhegor@gmail.com>
2022-04-16 12:46:47 +0300

    Second commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Task 4. Given a string where words are separated by any of the '_', '-', ' ' characters. Write a function that converts such string to single studly caps case word.
+For example
+Input:     The quick-brown_fox jumps over the_lazy-dog
+Output:    TheQuickBrownFoxJumpsOverTheLazyDog
+Func args:  (string $input)
--- Task4.php
+++ Task4.php
@@ -6,10 +6,14 @@ class Task4
 {
     public static function main($input): string
     {
+        if (!is_string($input)) {
+            throw new Exception('incorrect input data');
+        }
         $dash = str_replace('-', ' ', $input);
         $underline = str_replace('_', ' ', $dash);
         $bigLetter = ucwords($underline);
+
         return str_replace(' ', '', $bigLetter);
     }
 }
- Task4::main();
+//Task4::main();
