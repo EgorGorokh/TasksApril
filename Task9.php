8574c06c61b06bc83458cf22b63f67b25a392077
Egor Gorokh <gorokhegor@gmail.com>
2022-04-18 20:28:50 +0300

    Second committ

--- Task9.php
+++ Task9.php
@@ -2,26 +2,33 @@
 
 namespace Task;
 
-class Task10
+class Task9
 {
-    public static function main(int $input)
+    public static function main(array $arr,int $number)
     {
-        if (!is_int($input) || $input <= 1) {
+        if (!is_array($arr) || count($arr) < 3) {
             throw new Exception('incorrect input data');
         }
-        $count = 0; //счетчик
-        echo 'Array' . '<br>' . '(' . '<br>' . '[0] => ' . $input . '<br>';
-        while ($input !== 1) {
-            $count++;
-            if ($input % 2 == 0) {
-                $input = $input / 2;
-            } else {
-                $input = $input * 3 + 1;
-            }
-            echo '[' . $count . '] => ' . $input . '<br>';
+        if (!is_int($number)) {
+            throw new Exception('incorrect input data');
         }
-        echo ')';
+        $count = 0; //счетчик сколько таких сумм
+        for ($i = 0; $i < count($arr) - 2; $i++) {
+            if (($arr[$i] + $arr[$i + 1] + $arr[$i + 2]) == $number) {
+                $count++;
+            }
+        }
+        if ($count > 0) {
+            $count = 0; //обнуляем счетчик
+            echo 'Array' . '<br>' . '(' . '<br>';
+            for ($i = 0; $i < count($arr) - 2; $i++) {
+                if (($arr[$i] + $arr[$i + 1] + $arr[$i + 2]) == $number) {
+                    echo '[' . $count . '] ' . '=> ' . $arr[$i] . ' + ' . $arr[$i + 1] . ' + ' . $arr[$i + 2] .' = '.$number. '<br>';
+                    $count++;
+                }
+            }
+            echo ')';
+        }
     }
 }
-
-//echo Task10::main(55);
+Task9::main([2,7,7,1,8,2,7,8,7],16);
