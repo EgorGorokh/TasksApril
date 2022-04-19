6b04e38d337d1b79919b46d1f137e2608616d11f
Egor Gorokh <gorokhegor@gmail.com>
2022-04-19 17:04:52 +0300

    tfy

--- Task8.php
+++ Task8.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Task;
+namespace src;
 
 class Task8
 {
@@ -14,15 +14,14 @@ class Task8
             print_r("$key: $value<br>");
         }
         $books = json_decode($json, true);
-        array_walk_recursive($books, 'Task\bookArray');
-
+        array_walk_recursive($books, 'src\bookArray');
     }
 }
 
- /*Task8::main('{
+ Task8::main('{
 "Title": "The Cuckoos Calling",
 "Author": "Robert Galbraith",
 "Detail": {
 "Publisher": "Little Brown"
 }
-}');*/
+}');
