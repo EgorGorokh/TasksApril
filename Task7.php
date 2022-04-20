fae663fa0b0a973e3b7017f5810184e07f088a49
Egor Gorokh <82266177+EgorGorokh@users.noreply.github.com>
2022-04-20 16:41:27 +0300

    Update Task7.php

--- Task7.php
+++ Task7.php
@@ -7,16 +7,20 @@ class Task7
     public static function main(array $arr, int $position): array
     {
         if (!is_array($arr)) {
-            throw new Exception('incorrect input data');
+            throw new \InvalidArgumentException();
         }
-        if (!is_int($position)) {
-            throw new Exception('incorrect input data');
+        if (!is_int($position) || $position < 0) {
+            throw new \InvalidArgumentException();
         }
         if (count($arr) <= $position) {
-            throw new Exception('incorrect input data');
+            throw new \InvalidArgumentException();
         }
         unset($arr[$position]);
 
-        return array_values($arr);
+        // return array_values($arr);
+        //for ($i = 0; $i < count($arr); $i++) {
+        //    echo $arr[$i] . "<br>";
+        // }
+        return $arr;
     }
 }
