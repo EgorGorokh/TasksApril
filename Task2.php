210582b941caef81bc1c14640a15168846095fb2
Egor Gorokh <82266177+EgorGorokh@users.noreply.github.com>
2022-04-20 12:39:46 +0300

    Update Task2.php

--- Task2.php
+++ Task2.php
@@ -8,22 +8,24 @@ class Task2
     {
         $numbers = explode('-', $date);
 
-        if (!is_int($numbers[0]) || ($numbers[0]) < 1) {
+        if (($numbers[0]) < 1) {
             throw new InvalidArgumentException('incorrect input data');
         }
-        if (!is_int($numbers[1]) || ($numbers[1]) < 1 || ($numbers[1]) > 12) {
+        if (($numbers[1]) < 1 || ($numbers[1]) > 12) {
             throw new InvalidArgumentException('incorrect input data');
         }
-        if (!is_int($numbers[2]) || ($numbers[2]) < 1 || ($numbers[2]) > 31) {
+        if (($numbers[2]) < 1 || ($numbers[2]) > 31) {
             throw new InvalidArgumentException('incorrect input data');
         }
 
         $today = time();
         $diff = strtotime($date) - $today;
         if ($diff < 0) {
-            throw new Exception('incorrect input data');
+            throw new InvalidArgumentException('incorrect input data');
         }
 
         return abs(round($diff / 86400));
     }
 }
+
+//echo Task2::main('2022-05-16');
