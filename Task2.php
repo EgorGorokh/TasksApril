0260a965d7226d4c46f58e402de5d417b8919add
Egor Gorokh <82266177+EgorGorokh@users.noreply.github.com>
2022-04-20 12:23:48 +0300

    Update Task2.php

--- Task2.php
+++ Task2.php
@@ -1,15 +1,29 @@
 <?php
 
-namespace src;
+namespace Task;
 
 class Task2
 {
     public static function main(string $date): int
     {
+        $numbers = explode('-', $date);
+
+        if (!is_int($numbers[0]) || ($numbers[0]) < 1) {
+            throw new InvalidArgumentException('incorrect input data');
+        }
+        if (!is_int($numbers[1]) || ($numbers[1]) < 1 || ($numbers[1]) > 12) {
+            throw new InvalidArgumentException('incorrect input data');
+        }
+        if (!is_int($numbers[2]) || ($numbers[2]) < 1 || ($numbers[2]) > 31) {
+            throw new InvalidArgumentException('incorrect input data');
+        }
+
         $today = time();
         $diff = strtotime($date) - $today;
+        if ($diff < 0) {
+            throw new Exception('incorrect input data');
+        }
 
         return abs(round($diff / 86400));
     }
 }
-//echo Task2::main('16-05-2022');
