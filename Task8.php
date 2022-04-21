0fc9b27e5d4e75c4bb04910d3eee128bf7040209
Egor Gorokh <82266177+EgorGorokh@users.noreply.github.com>
2022-04-21 14:55:26 +0300

    Update Task8.php

--- Task8.php
+++ Task8.php
@@ -4,77 +4,51 @@ namespace src;
 
 class Task8
 {
-    public static function main(string $json)
+    public function main(string $json): string
     {
-        if (!is_string($json) && !is_array(json_decode($json, true))) {
-            throw new \InvalidArgumentException();
+        if (!$this->isJSON($json)) {
+            return throw new \InvalidArgumentException();
         }
-        /*
-                function bookArray($value, $key)
-                {
-                    global $st;
-                    global $str;
-                    $str = $key . ': ' . $value;
-                    $st = $st . $str . '<br>';
-                    // echo $str . "<br>";
-                    // echo $str . '<br>';
-                    // print_r("$key: $value<br>");
-                    // echo '<br>'.$st.'<br>';
-                    // echo $st;
-                }
-        */
-        //echo $st;
+        $books = json_decode($json, true);
 
-        //  $books = json_decode($json, true);
-        $books = json_decode($json);
-        //array_walk_recursive($books, 'src\bookArray');
-        //  foreach ($books as $key => $value) {
-        // echo $key;
-        //  echo "<br>";
-        // }
-        //  foreach ($books->array[0] as $key => $values) {
-        //   echo "$key:$values<br>";
-        //  }
 
         $st = '';
         foreach ($books as $field => $value) {
-            //если скалярное значение
             if (is_scalar($value) or is_null($value)) {
-                // echo $field . ' : ' . $value . '</br>';
                 $st = $st . $field . ': ' . $value."\r\n";
             }
-            //если массив
+
             if (is_array($value)) {
-                for ($i = 0; $i < count($value); $i++) {
-                    //а в нем у нас объекты
-                    foreach ($value[$i] as $field_ob => $value_ob) {
-                        // echo  $field_ob . ' -> ' . $value_ob . '</br>';
-                    }
+                foreach ($value as $key => $item) {
+                    $st = $st . $key . ': ' . $item."\r\n";
                 }
             }
-            //если другой объект
+
             if (is_object($value)) {
                 foreach ($value as $field_ob => $value_ob) {
-                    //  echo/* $field . ' -> ' .*/ $field_ob . ' : ' . $value_ob . '</br>';
-                    $st = $st . $field_ob . ': ' . $value_ob;
+                    $st .= $st . $field_ob . ': ' . $value_ob."\r\n";
                 }
             }
         }
-        // echo '<br>'.$st.'<br>';
 
         return $st;
-        //print_r($books);
-        //  echo '<br>';
-        // echo $st;
-        //echo '<br>';
-        //  $books = json_encode($books, true);
-        // print_r($books);
+    }
+
+    public function isJSON($string): bool
+    {
+        return is_string($string) && is_array(json_decode($string, true)) ? true : false;
     }
 }
- echo Task8::main('{
+/*
+print_r((new Task8)->main('{
 "Title": "The Cuckoos Calling",
 "Author": "Robert Galbraith",
 "Detail": {
 "Publisher": "Little Brown"
 }
-}');
+}'));*/
+/*
+print_r((new Task8)->main('{
+1
+}
+'));*/
