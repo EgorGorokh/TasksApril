86cdd75ab2f769f6919d0aa8109f9a4be8183dbd
Egor Gorokh <82266177+EgorGorokh@users.noreply.github.com>
2022-04-21 15:03:16 +0300

    Update Task8.php

--- Task8.php
+++ Task8.php
@@ -16,17 +16,19 @@ class Task8
         foreach ($books as $field => $value) {
             if (is_scalar($value) or is_null($value)) {
                 $st = $st . $field . ': ' . $value."\r\n";
+                // echo"3";
             }
 
             if (is_array($value)) {
                 foreach ($value as $key => $item) {
-                    $st = $st . $key . ': ' . $item."\r\n";
+                    $st = $st . $key . ': ' . $item;
                 }
             }
 
             if (is_object($value)) {
                 foreach ($value as $field_ob => $value_ob) {
                     $st .= $st . $field_ob . ': ' . $value_ob."\r\n";
+                    //echo 'jhjhv';
                 }
             }
         }
@@ -46,7 +48,9 @@ print_r((new Task8)->main('{
 "Detail": {
 "Publisher": "Little Brown"
 }
-}'));*/
+}'));
+*/
+
 /*
 print_r((new Task8)->main('{
 1
