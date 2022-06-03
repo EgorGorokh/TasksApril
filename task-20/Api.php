ad2c8677b76400360bf77a49228c20380521effb
Egor Gorokh <gorokhegor@gmail.com>
2022-06-03 15:21:56 +0300

    Firsttttt

--- task-20/Api.php
+++ task-20/Api.php
@@ -2,6 +2,6 @@
 
 class Api
 {
-    public static $token = '4bdac3a5aaf0da94a3f9c11c7e7d02c97a33ab34a28703f2e42a2a36bc6ba883';
+   // public static $token = '4bdac3a5aaf0da94a3f9c11c7e7d02c97a33ab34a28703f2e42a2a36bc6ba883';
     public static $path = 'https://gorest.co.in/public/v2/users';
 }
